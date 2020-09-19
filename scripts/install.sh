ec6454485155e2a5bdf2b6eba7d60dae6371e073
Mayeu <m@mayeu.me>
2020-09-19 14:49:57 +0200

    ♻️  Small simplification of the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-if [ ! -d "$PWD/bin" ]; then
-  mkdir bin
-fi
+mkdir -p bin
 
 if [ "$(uname)" == "Darwin" ]; then
-  curl -L https://github.com/Awea/pretty-make/releases/latest/download/pretty-make-macos -o pretty-make
+  curl -L https://github.com/Awea/pretty-make/releases/latest/download/pretty-make-macos -o bin/pretty-make
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  curl -L https://github.com/Awea/pretty-make/releases/latest/download/pretty-make-linux -o pretty-make
+  curl -L https://github.com/Awea/pretty-make/releases/latest/download/pretty-make-linux -o bin/pretty-make
 fi
 
-chmod 755 pretty-make
-mv pretty-make bin/pretty-make
+chmod 755 bin/pretty-make
