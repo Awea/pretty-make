594aa778ce839485d4e50e2d15d6bc76b77c5a6f
Awea <aweaoftheworld@gmail.com>
2021-06-17 01:53:37 +0200

    🚧 Switch to Clap 3
    - Trying auto-completion

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate clap;
+extern crate clap_generate;
 extern crate colored;
 extern crate colors_transform;
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg};
+use clap_generate::{generate, generators::Bash};
+use std::io;
 
 #[path = "pretty-make/mod.rs"]
 mod pretty_make;
@@ -16,20 +19,26 @@ const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
 const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 
 fn main() {
-    let matches = App::new("Pretty Make")
+    let mut app = App::new("Pretty Make")
         .version(VERSION)
         .author(AUTHORS)
         .about(DESCRIPTION)
-        .subcommands(vec![
-            SubCommand::with_name("pretty-help")
+        .subcommand(
+            App::new("pretty-help")
                 .about("Parse a Makefile to produce a pretty help.")
                 .arg(
-                    Arg::with_name("makefile")
-                        .help("Makefile to be pretty")
+                    Arg::new("makefile")
+                        .about("Makefile to be pretty")
                         .required(true),
                 ),
-        ])
-        .get_matches();
+        );
+
+    // Auto-completion is a feature in clap 3 but it doesn't work yet
+    // - @awea 20210617
+    // Issues: https://github.com/clap-rs/clap/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc+completion
+    // generate::<Bash, _>(&mut app, "Pretty Make", &mut io::stdout());
+
+    let matches = app.get_matches();
 
     if let Some(ref matches) = matches.subcommand_matches("pretty-help") {
         if let Some(makefile) = matches.value_of("makefile") {
